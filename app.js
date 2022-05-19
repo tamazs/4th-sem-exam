3e5be12b96bdff5a25f78b8b1751becfac0a93ca
tamazs <majortamas313@gmail.com>
2022-05-19 22:49:54 +0200

    video scroll animation added

--- app.js
+++ app.js
@@ -70,3 +70,26 @@ const tlSplit = gsap.timeline({
       end: "100%",
     },
   });
+
+//Page 5 video on scroll
+const tlVideo = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".fifth-page",
+      start: "0%",
+      end: "150%",
+      scrub: true,
+      pin: true,
+    },
+  });
+  tlVideo.fromTo(
+    ".product-video",
+    { currentTime: 0 },
+    { currentTime: 3, duration: 1 }
+  );
+  
+  tlVideo.fromTo(
+    ".product-info-container h3",
+    { opacity: 0 },
+    { opacity: 1, stagger: 0.25, duration: 0.5 },
+    "<"
+  );
